94509af89323441edb0aa1b86dd9752e8a9548a8
Amir-Muktar <erkingulbegalieva4@gmail.com>
2024-12-12 23:59:14 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/aboutPage/aboutPage";
+import TodosPage from "./pages/todosPage/todosPage";
+
+
+
+function App() {
+    return (
+        <>
+            <MainPage/>
+            <AboutPage/>
+            <TodosPage/>
+        </>
+    );
+}
+
+export default App;
